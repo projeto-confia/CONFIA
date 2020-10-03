e22a596234eca9282b46e6d3b89218741ab7cd43
Gabriel <gabriel.rmachado10@gmail.com>
2020-10-03 00:10:28 -0300

    Primeiro Commit

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import tweepy
+import stream
+import authconfig as cfg
 
-if __name__ == "__main__":
-
-    consumer_key = "henwvehuKH6MQtzUbYtGklAWf"
-    consumer_secret = "KunYaNThhddSTqTWt6TLSxdkfBIiFoxKrk8MgF5OUqdgDrqSWh"
-    access_token = "1176207436119793666-kSr7xfmkR3FCP0XBroqc3AYE8ucGRn"
-    access_token_secret = "afN1DR6SGh7JFqQZfAeqz3nnu4ZwZ76rAS44F8u4c6Sh4"
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+def primeiroAcesso():
+    tokens = cfg.tokens
+    auth = tweepy.OAuthHandler(tokens["consumer_key"], tokens["consumer_secret"])
+    auth.set_access_token(tokens["access_token"], tokens["access_token_secret"])
 
     api = tweepy.API(auth)
     public_tweets = api.home_timeline()
 
     for tweet in public_tweets:
         print(tweet.text)
+
+def exemploStreaming():
+    tokens = cfg.tokens
+    streamListener = stream.StreamListener()
+    auth = tweepy.OAuthHandler(tokens["consumer_key"], tokens["consumer_secret"])
+    auth.set_access_token(tokens["access_token"], tokens["access_token_secret"])
+
+    api = tweepy.API(auth)
+    streamAccess = tweepy.Stream(auth=api.auth, listener=streamListener)
+    streamAccess.filter(track=["COVID"], languages=["en", "pt"])
+
+if __name__ == "__main__":
+    exemploStreaming()
+    
+
+    
