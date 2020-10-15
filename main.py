4fc7bf636fc191b30a10875aca994d5d1b6930b9
Gabriel <gabriel.rmachado10@gmail.com>
2020-10-15 13:00:34 -0300

    Comparando usuarios do Twitter com a base de usuarios users.csv

--- main.py
+++ main.py
@@ -1,34 +1,6 @@
-import tweepy
-import stream
-import authconfig as cfg
-
-def primeiroAcesso():
-    tokens = cfg.tokens
-    auth = tweepy.OAuthHandler(tokens["consumer_key"], tokens["consumer_secret"])
-    auth.set_access_token(tokens["access_token"], tokens["access_token_secret"])
-
-    api = tweepy.API(auth)
-    followers = api.followers()
-    api.send_direct_message(recipient_id=3243420285, text="Isso é um teste!")
-
-    for f in followers:
-        print(f.screen_name+ " - " + f.name + " - " + str(f.id))
-    # for f in followers:
-    #     print(f.author.name)
-
-def exemploStreaming():
-    tokens = cfg.tokens
-    streamListener = stream.StreamListener()
-    auth = tweepy.OAuthHandler(tokens["consumer_key"], tokens["consumer_secret"])
-    auth.set_access_token(tokens["access_token"], tokens["access_token_secret"])
-
-    api = tweepy.API(auth)
-    streamAccess = tweepy.Stream(auth=api.auth, listener=streamListener)
-    streamAccess.filter(track=["COVID", "covid", "Covid",  "coronavirus", "coronavírus", "covid-19"], languages=["pt"])
+from Streaming import stream
 
 if __name__ == "__main__":
-    primeiroAcesso()
-    # exemploStreaming()
-
-    # 3243420285
-    # 3243420285
+    streaming = stream.Streaming()
+    streaming.runStreaming()
+    
--- stream.py
+++
@@ -1,8 +0,0 @@
-import tweepy
-
-# class Streaming:
-
-
-class StreamListener(tweepy.StreamListener):
-    def on_status(self, status):
-        print("ID: {0} - @{1} - {2}\n".format(status.author.id, status.author.screen_name, status.text))
