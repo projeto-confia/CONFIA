9339a7bb27878fb5c58af42cd5aa4bda66a3835f
Augusto Jose Moreira da Fonseca <ajosemf@gmail.com>
2020-10-27 23:31:47 -0300

    Estruturando o projeto pela convençao pep8

---
+++ confia/engine/engine.py
@@ -0,0 +1,34 @@
+from confia.collector.stream import Streaming
+
+
+class Engine(object):
+    """
+    docstring
+    """
+
+    def __init__(self):
+        # load json
+        # start logger
+        pass
+
+
+    def restart(self):
+        """
+        docstring
+        """
+        # load json
+
+
+    def run(self):
+        """
+        docstring
+        """
+        self.collect()
+
+    
+    def collect(self):
+        """
+        docstring
+        """
+        streaming = Streaming()
+        streaming.run()
