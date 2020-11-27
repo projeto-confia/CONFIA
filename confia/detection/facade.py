7bce9586a807599008945d6dbe5dd6c2a297069f
Gabriel <gabriel.rmachado10@gmail.com>
2020-11-27 20:25:51 -0300

    Primeira versao do modulo de deteccao foi implementada.

--- confia/detection/facade.py
+++ confia/detection/facade.py
@@ -2,4 +2,7 @@ from confia.detection.detector import Detector
 
 class DetectorFacade:
     def run(self):
-        d = Detector()
+        try:
+            d = Detector()
+        except Exception as e:
+            print(str(e))
