9bb69a917037b54751bcffe5b2a005965481cb8a
Gabriel Machado <gabriel.rmachado10@gmail.com>
2021-01-27 19:47:09 -0300

    Lematizador implementado no modulo de pre-processamento.

--- confia/monitor/preprocessing.py
+++ confia/monitor/preprocessing.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import nltk
+import re
+import string
 from googletrans import Translator
 from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
 from nltk.tag import pos_tag
 from nltk.stem.wordnet import WordNetLemmatizer
+
 
 class TextPreprocessing:
 
@@ -13,15 +17,16 @@ class TextPreprocessing:
         Necessária como pré-requisito para a análise de sentimento do texto.
         """
         self.__check_nltk_packages(install_nltk_packages)
-        self.__translator   = Translator()
-        self.__tokenizer    = TweetTokenizer()
-        self.__lemmatizer   = WordNetLemmatizer()
+        self.__translator = Translator()
+        self.__tokenizer = TweetTokenizer()
+        self.__lemmatizer = WordNetLemmatizer()
 
     def __check_nltk_packages(self, install_nltk_packages):
         if install_nltk_packages == True:
             nltk.download('punkt')
             nltk.download('averaged_perceptron_tagger')
             nltk.download('wordnet')
+            nltk.download('stopwords')
 
     def translate(self, tweet):
         """
@@ -34,21 +39,31 @@ class TextPreprocessing:
 
     def lemmatizer(self, tokens):
         lemmatized_text = []
-        
-        for word, tag in pos_tag(tokens):
+
+        # remove hyperlinks, nomes de usuário precedidos pelo '@', pontuações e caracteres especiais.
+        for token, tag in pos_tag(tokens):
+            token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'
+                           '(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', token, flags=re.MULTILINE)
+            token = re.sub("(@[A-Za-z0-9_]+)", "", token, flags=re.MULTILINE)
+
             if tag.startswith('NN'):
                 pos = 'n'
             elif tag.startswith('VB'):
                 pos = 'v'
             else:
                 pos = 'a'
-            lemmatized_text.append(self.__lemmatizer.lemmatize(word, pos))
-        
+
+            token = self.__lemmatizer.lemmatize(token, pos)
+
+            # verifica se o token (palavra) não é vazia ou irrelevante antes de adicioná-la à 'lemmatized_text'.
+            if len(token) > 0 and token not in string.punctuation and token.lower() not in stopwords.words('english'):
+                lemmatized_text.append(token.lower())
+
         return lemmatized_text
 
-    def print_preprocessing(self, tweet):
-        tweet           = self.translate(tweet)
-        tokens          = self.tokenize(tweet)
+    def process_text(self, tweet):
+        tweet = self.translate(tweet)
+        tokens = self.tokenize(tweet)
         lemmatized_text = self.lemmatizer(tokens)
 
         print("\nTweet processado: {0}\n".format(lemmatized_text))
