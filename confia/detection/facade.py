ad678837c5cbd412f5cc06911aa8a237aa66e086
Augusto Jose Moreira da Fonseca <ajosemf@gmail.com>
2021-05-25 16:48:34 -0300

    Liga o treinamento do ICS

--- confia/detection/facade.py
+++ confia/detection/facade.py
@@ -2,7 +2,7 @@ from confia.detection.detector import Detector
 
 
 class DetectorFacade:
-    def __init__(self, train_ics=False):
+    def __init__(self, train_ics=True):
         self.__d = Detector(train_ics)
 
     def run(self):
