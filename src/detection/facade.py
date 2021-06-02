4abcc9f0e7866359a6cda6557202933388679274
Gabriel Machado <gabriel.rmachado10@gmail.com>
2021-06-02 14:52:33 -0300

    Alteraçao da fachada do Detector.

--- src/detection/facade.py
+++ src/detection/facade.py
@@ -1,9 +1,19 @@
 from src.detection.detector import Detector
-
+from src.config import Config as config
+import logging
 
 class DetectorFacade:
-    def __init__(self, train_ics=True):
-        self.__d = Detector(train_ics)
+    """
+    docstring
+    """
+    def __init__(self):
+        self._logger = logging.getLogger(config.LOGGING.NAME)
 
     def run(self):
-        self.__d.predict_news(40)
+        try:
+            self._logger.info('Running Detector...')
+            self._detector = Detector(config.DETECTOR.TRAIN_ICS)
+            self._logger.info('Detector finished.')
+        except:
+            raise
+        
