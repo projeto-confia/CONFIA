dbd7ec14ab10614ac75f6e7f4fc8a3d5fc00c406
Augusto Jose Moreira da Fonseca <ajosemf@gmail.com>
2021-06-08 20:30:10 -0300

    Adiciona classe generica

--- src/scraping/facade.py
+++ src/scraping/facade.py
@@ -1,4 +1,4 @@
-from src.scraping.scraping import Scraping
+from src.scraping.scraping import ScrapingBoatosOrg
 import logging
 from src.config import Config as config
 
@@ -15,7 +15,7 @@ class ScrapingFacade(object):
     def run(self):
         try:
             self._logger.info('Running Scraping...')
-            Scraping().run()
+            ScrapingBoatosOrg().run()
             self._logger.info('Scraping finished.')
         except:
             raise
