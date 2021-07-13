7f2b3090a487e1ebd7b911fa36affe6172582120
Augusto Jose Moreira da Fonseca <ajosemf@gmail.com>
2021-07-12 21:17:31 -0300

    Carrega lista de id_news rotuladas como fake pela ACF

--- src/fcmanager/fact_check_manager.py
+++ src/fcmanager/fact_check_manager.py
@@ -1,12 +1,13 @@
 import logging
 from src.config import Config as config
+from src.fcmanager.dao import FactCheckManagerDAO
 
 
 class FactCheckManager(object):
     
     def __init__(self):
         self._logger = logging.getLogger(config.LOGGING.NAME)
-        # other inicial actions
+        self._dao = FactCheckManagerDAO()
         self._logger.info('FactCheckManager initialized.')
         
         
@@ -16,7 +17,25 @@ class FactCheckManager(object):
     
     
     def process_agency_feed(self):
+        
+        # If has not excel file, stop process
+        if not self._dao.has_excel_file():
+            return
+        
+        fakenews_ids = self._dao.get_fakenews_ids_from_excel()
+
+        # if has no records, stop process
+        if not fakenews_ids:
+            return
+        
+        # update filtered records into database
+            # if some id record doesn't exist in the database table, register occurrence in the log
+        
+        # move excel file to ./data/acf/processed folder
+        
+        
         self._logger.info('Processing feed from agency...')
+        
         
     
     def persist_data(self):
