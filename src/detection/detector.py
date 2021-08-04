169e879ddc31fdd351236bba1310b4a7c5ecd425
Gabriel Machado <gabriel.rmachado10@gmail.com>
2021-08-04 19:22:28 -0300

    Reparo do bug de prediçao de noticias compartilhadas por veiculos de imprensa.

--- src/detection/detector.py
+++ src/detection/detector.py
@@ -21,27 +21,30 @@ class Detector:
         try:
             # verificando se usuários com reputação (i.e. usuários cujos parâmetros 'prob' diferente de 0.5) compartilharam alguma nova notícia.
             news_shared_by_users_with_params_ics = self.__dao.get_news_shared_by_users_with_params_ics()
+            print(news_shared_by_users_with_params_ics)
 
             if len(news_shared_by_users_with_params_ics) == 0:
                 self.__logger.info("Nenhuma nova notícia compartilhada por usuários reputados.")
             else:
-                self.__logger.info("Atualizando notícias compartilhadas por usuários reputados...")
                 news = []
                 
                 for _, row in news_shared_by_users_with_params_ics.iterrows():
                     id_news = row["id_news"]
-                    predicted_label, prob_label = self.predict_news(id_news)
-                    ground_truth_label = row["ground_truth_label"] 
-                    news.append(id_news)
+                    
+                    if row['ground_truth_label'] == None: # classifica com o ICS apenas as notícias não produzidas por veículos de imprensa.
+                        predicted_label, prob_label = self.predict_news(id_news)
+                        ground_truth_label = row["ground_truth_label"] 
+                        news.append(id_news)
 
-                    # atualiza os labels da notícia 'id_news'.
-                    self.__dao.update_news_labels(id_news, bool(predicted_label), ground_truth_label, prob_label)
-                
-                self.__logger.info("As seguintes notícias foram atualizadas:")
-                
-                for i in range(len(news)-1):
-                    self.__logger.info(f"{news[i]}, ")
-                self.__logger.info(f"{news[len(news)-1]}.")
+                        # atualiza os labels da notícia 'id_news'.
+                        self.__dao.update_news_labels(id_news, bool(predicted_label), ground_truth_label, prob_label)
+
+                if len(news):
+                    self.__logger.info("As seguintes notícias foram atualizadas:")
+                    
+                    for i in range(len(news)-1):
+                        self.__logger.info(f"{news[i]}, ")
+                    self.__logger.info(f"{news[len(news)-1]}.")
 
         except Exception as e:
             self.__logger.error(f"Ocorreu um erro durante a atualização das notícias: {e.args}")
