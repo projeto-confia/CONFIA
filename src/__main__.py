12604ff5977f6cccd18abb9c024aa13257326769
Gabriel Machado <gabriel.rmachado10@gmail.com>
2021-08-20 16:45:14 -0300

    Correçao de bugs no treinamento do ICS.

--- src/__main__.py
+++ src/__main__.py
@@ -16,15 +16,15 @@ def init_log(verbose=False):
     logger.addHandler(file_handler)
     
     # smtp handler
-    smtp_handler = logging.handlers.SMTPHandler(mailhost=('smtp.gmail.com', 587),
-                                                fromaddr=config.EMAIL.ACCOUNT,
-                                                toaddrs=config.EMAIL.ACCOUNT,
-                                                subject='Log Alert',
-                                                credentials=(config.EMAIL.ACCOUNT, config.EMAIL.PASSWORD),
-                                                secure=())
-    smtp_handler.setLevel(logging.WARNING)
-    smtp_handler.setFormatter(file_format)
-    logger.addHandler(smtp_handler)
+    # smtp_handler = logging.handlers.SMTPHandler(mailhost=('smtp.gmail.com', 587),
+    #                                             fromaddr=config.EMAIL.ACCOUNT,
+    #                                             toaddrs=config.EMAIL.ACCOUNT,
+    #                                             subject='Log Alert',
+    #                                             credentials=(config.EMAIL.ACCOUNT, config.EMAIL.PASSWORD),
+    #                                             secure=())
+    # smtp_handler.setLevel(logging.WARNING)
+    # smtp_handler.setFormatter(file_format)
+    # logger.addHandler(smtp_handler)
     
     if verbose:
         stream_format = logging.Formatter('%(levelname)s - %(message)s')
