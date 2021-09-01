743f2762fb5aff1f5e466be937741513f03b4e02
Augusto Jose Moreira da Fonseca <ajosemf@gmail.com>
2021-09-01 18:24:09 -0300

    Remove imports, atributos e metodos nao utilizados

--- src/monitor/dao.py
+++ src/monitor/dao.py
@@ -1,120 +1,27 @@
+import numpy as np
+import pandas as pd
+import multiprocessing as mp
+import os, math
 from operator import itemgetter
+from datetime import datetime, timedelta
 from src.config import Config as config
 from src.orm.db_wrapper import DatabaseWrapper
 from src.utils.text_preprocessing import TextPreprocessing
-import multiprocessing as mp
-import csv, os, math
-import numpy as np
-import pandas as pd
-from datetime import datetime, timedelta
 
 
 class MonitorDAO(object):
-    """Funcionalidades DAO voltadas para o módulo de monitoramento. 
+    """DAO functionalities to support monitor module
 
     Args:
-        batch_size (int): o tamanho do batch que será lida do arquivo de texto de notícias recuperado do banco de dados para comparação (deduplicação). 
+        object (object): Python base class
     """
 
     def __init__(self):
 
-        self._tweet_csv_header = ['name_social_media', 'id_account_social_media', 'screen_name',
-                                  'date_creation', 'blue_badge', 'id_post_social_media', 
-                                  'parent_id_post_social_media', 'text_post', 'num_likes', 'num_shares', 'datetime_post']
-        
-        self._text_processor = TextPreprocessing()
-        self._tweet_csv_filename = 'tweets.csv'
-        self._tweet_csv_path = os.path.join("src", "data", self._tweet_csv_filename)
-        self._tweet_pkl_filename = 'tweets.pkl'
-        self._tweet_pkl_path = os.path.join("src", "data", self._tweet_pkl_filename)
-        self._id_social_media = None
+        self._tweet_file = 'tweets.pkl'
+        self._tweet_filepath = os.path.join("src", "data", self._tweet_file)
         self._text_news_cleaned = self._get_text_news_cleaned()
       
-
-    def insert_posts(self):
-        """
-        docstring
-        """
-        
-        # TODO: incluir testes de existência do arquivo e existẽncia de registros (file_exists, has_data)
-
-        # carrega o arquivo csv
-        data = self._load_csv_to_dict(self._tweet_csv_path, fieldnames=self._tweet_csv_header, delimiter=';')
-
-        # inicia a transação
-        try:
-            with DatabaseWrapper() as db:                
-                for post in data:
-                    news_with_biggest_score = [-1, -1, False]
-                    
-                    # separa os dados por tabela
-                    social_media_data = {k:post[k] for k in list(self._tweet_csv_header[:1])}
-                    account_data = {k:post[k] for k in list(self._tweet_csv_header[1:5])}
-                    post_data = {k:post[k] for k in list(self._tweet_csv_header[5:])}
-                    news_data = {'text_news': post['text_post'],
-                                 'datetime_publication': post['datetime_post']}
-
-                    # consulta se a pessoa já possui registro de conta na rede social
-                    id_social_media_account = self._get_id_social_media_account(account_data, db)
-                    if not id_social_media_account:
-                        # insere ou recupera o id da pessoa que publicou o post
-                        # TODO: verificar se vamos implementar
-
-                        # insere ou recupera o id da rede social
-                        # TODO: implementar e substituir o bloco abaixo
-
-                        # recupera o id da rede social
-                        if self._id_social_media is None:
-                            self._id_social_media = self._get_id_social_media(social_media_data, db)
-                        
-                        # insere a conta e recupera o id
-                        account_data['id_social_media'] = self._id_social_media
-                        account_data["probalphan"] = 0.5
-                        account_data["probbetan"] = 0.5
-                        account_data["probumalphan"] = 0.5
-                        account_data["probumbetan"] = 0.5
-                        id_social_media_account = self._insert_record('detectenv.social_media_account',
-                                                                      account_data,
-                                                                      'id_social_media_account',
-                                                                      db)
-
-                    # consulta se a notícia já possui registro no banco
-
-                    if len(self._text_news_cleaned): # verifica se a tabela 'detectenv.news' está vazia.
-                        news_with_biggest_score = read_cleaned_news_db_in_parallel(news_data, self._text_news_cleaned)
-                    
-                    if not news_with_biggest_score[2]:
-                        # insere a notícia e recupera o id
-                        id_news = self._insert_record('detectenv.news',
-                                                      news_data,
-                                                      'id_news',
-                                                      db)
-                    else:
-                        id_news = news_with_biggest_score[0]
-
-                    # insere o post
-                    post_data['id_social_media_account'] = id_social_media_account
-                    post_data['id_news'] = int(id_news)
-                    
-                    if not post_data['parent_id_post_social_media']:
-                        post_data['parent_id_post_social_media'] = None
-                    
-                    self._insert_record('detectenv.post',
-                                        post_data,
-                                        'id_post',
-                                        db)
-
-            # deleta o arquivo csv ou registra no log (e-mail) caso negativo
-            os.remove(self._tweet_csv_path)
-
-        except:
-            raise
-
-    def write_in_csv_from_dict(self, data):
-        with open(self._tweet_csv_path, mode='a') as f:
-            writer = csv.DictWriter(f, data.keys(), delimiter=';')
-            writer.writerow(data)
-            
             
     def write_in_pkl(self, df):
         """Persist data within a pickle file
@@ -124,7 +31,7 @@ class MonitorDAO(object):
         """
         if not isinstance(df, pd.DataFrame):
             df = pd.DataFrame(df)
-        df.to_pickle(self._tweet_pkl_path)
+        df.to_pickle(self._tweet_filepath)
             
             
     def get_media_accounts(self, name_social_network):
@@ -144,6 +51,26 @@ class MonitorDAO(object):
 
         with DatabaseWrapper() as db:
             return db.query(sql_string, (name_social_network,))
+        
+        
+    def get_last_media_post(self, id_social_media_account):
+        """Recupera o maior datetime de publicação de post
+
+        Args:
+            id_social_media_account (int): Id da conta da media na rede social
+
+        Returns:
+            datetime: Maior datetime de publicação armazenado no banco
+        """
+        sql_string = "SELECT MAX(p.datetime_post) \
+                      FROM detectenv.post p \
+                      WHERE p.id_social_media_account = %s;"
+        try:
+            with DatabaseWrapper() as db:
+                record = db.query(sql_string, params=(id_social_media_account,))
+                return record[0][0]
+        except:
+            raise
     
     
     def insert_media_posts(self):
@@ -171,7 +98,7 @@ class MonitorDAO(object):
                     id_news_list = self._insert_news_in_db(df_news, db, has_ground_truth_label=True)
                     self._update_dataframe_with_id_news(df_news, id_news_list, df, groups)
                 self._insert_posts_in_db(df, db)
-            os.remove(self._tweet_pkl_path)
+            os.remove(self._tweet_filepath)
         except:
             raise
         
@@ -201,7 +128,7 @@ class MonitorDAO(object):
                     self._update_dataframe_with_id_news(df_news, id_news_list, df, groups)
                 df = self._update_dataframe_with_social_network_accounts(df, db)
                 self._insert_posts_in_db(df, db)
-            os.remove(self._tweet_pkl_path)
+            os.remove(self._tweet_filepath)
         except:
             raise
             
@@ -250,7 +177,7 @@ class MonitorDAO(object):
          
     def _update_dataframe_with_social_network_accounts(self, df:pd.DataFrame, db:DatabaseWrapper):
         social_network_data = {'name_social_media': df['name_social_media'].unique()[0]}
-        id_social_network = self._get_id_social_media(social_network_data, db)
+        id_social_network = self._get_id_social_network(social_network_data, db)
         external_accounts = df['id_account_social_media'].unique()
         internal_accounts = self._get_social_networks_accounts(id_social_network,
                                                                external_accounts)
@@ -319,26 +246,6 @@ class MonitorDAO(object):
                                     arglist,
                                     'id_post',
                                     db)
-    
-        
-    def get_last_media_post(self, id_social_media_account):
-        """Recupera o maior datetime de publicação de post
-
-        Args:
-            id_social_media_account (int): Id da conta da media na rede social
-
-        Returns:
-            datetime: Maior datetime de publicação armazenado no banco
-        """
-        sql_string = "SELECT MAX(p.datetime_post) \
-                      FROM detectenv.post p \
-                      WHERE p.id_social_media_account = %s;"
-        try:
-            with DatabaseWrapper() as db:
-                record = db.query(sql_string, params=(id_social_media_account,))
-                return record[0][0]
-        except:
-            raise
 
 
     def _load_pkl(self, filepath=None) -> pd.DataFrame:
@@ -352,7 +259,7 @@ class MonitorDAO(object):
         """
         try:
             if not filepath:
-                filepath = self._tweet_pkl_path
+                filepath = self._tweet_filepath
             if not os.path.exists(filepath):
                 return None
             return pd.read_pickle(filepath)
@@ -360,23 +267,6 @@ class MonitorDAO(object):
             raise
         
         
-    def _load_csv_to_dict(self, file_path, fieldnames, delimiter=','):
-        """
-        Carrega um arquivo csv
-
-        Retorno
-        --------
-        data: list of dicts
-        """
-
-        data = list()
-        with open(file_path) as f:
-            reader = csv.DictReader(f, fieldnames=fieldnames, delimiter=delimiter)
-            for row in reader:
-                data.append(row)
-        return data
-
-
     def _insert_record(self, tablename, data, returning, db):
         """
         Insere um registro no banco de dados
@@ -435,53 +325,21 @@ class MonitorDAO(object):
         return returnings
 
 
-    # TODO: refatorar para função genéria _get_id_record
-    def _get_id_social_media_account(self, ac_data, db):
-        """
-        Recupera o id da conta na rede social
+    # TODO: refactor to generic function and moves to orm module
+    def _get_id_social_network(self, social_network_data:dict, db:DatabaseWrapper):
+        """Fetch social network's id from database
 
-        Parâmetros
-        -----------
-        ac_data: dict
-            Dicionário contendo os dados da conta na rede social
+        Args:
+            social_network_data (dict): Dict that must have 'name_social_media' key
+            db (DatabaseWrapper): Database connection instance
 
-        db: DatabaseWrapper
-            Instância de conexão com o banco de dados
-
-        Retorno
-        ----------
-        id: int
-            id se já está registrado, 0 caso contrário
-        """
-
-        sql_string = "SELECT id_social_media_account from detectenv.social_media_account where id_account_social_media = %s;"
-        arg = ac_data['id_account_social_media']
-        record = db.query(sql_string, (arg,))
-        return 0 if not len(record) else record[0][0]
-
-
-    def _get_id_social_media(self, sm_data, db):
-        """
-        Recupera o id da rede social
-
-        Parâmetros
-        -----------
-        sm_data: dict
-            Dicionário contendo os dados da rede social
-
-        db: DatabaseWrapper
-            Instância de conexão com o banco de dados
-
-        Retorno
-        ----------
-        id: int
-            id se já está registrado, 0 caso contrário
+        Returns:
+            int: Social network's id
         """
 
         sql_string = "SELECT id_social_media from detectenv.social_media where upper(name_social_media) = upper(%s);"
-        arg = sm_data['name_social_media']
+        arg = social_network_data['name_social_media']
         record = db.query(sql_string, (arg,))
-
         return 0 if not len(record) else record[0][0]
     
     
@@ -496,32 +354,6 @@ class MonitorDAO(object):
                 return records
         except:
             raise
-        
-    
-    def clean_and_save_text_news(self):
-        with DatabaseWrapper() as db:
-            no_cleaned_news = self._get_no_cleaned_text_news(db)
-
-            if no_cleaned_news != False:
-                for news in no_cleaned_news:
-                    news_aux = [news[0], news[1]]
-                    news_aux[1] = self._text_processor.text_cleaning(news[1])
-                    self._update_no_cleaned_news_with_cleaned_text(news_aux, db)
-                return True
-
-            return False
-
-
-    def _get_no_cleaned_text_news(self, db):
-        sql_string = "select id_news, text_news from detectenv.news where text_news_cleaned is null;"
-        no_cleaned_news = db.query(sql_string)
-        return False if not len(no_cleaned_news) else no_cleaned_news
-
-
-    def _update_no_cleaned_news_with_cleaned_text(self, news, db):
-        sql_string = "update detectenv.news set text_news_cleaned = %s where id_news = %s;"
-        db.execute(sql_string, (news[1], news[0],))
-        db.commit()
 
 
 def read_cleaned_news_db_in_parallel(news_data, cleaned_news_db):
@@ -559,7 +391,6 @@ def _get_indices_batches_news_db(total_news_db, batch_size = 128):
         list: uma lista contendo os índices pertencentes ao intervalo (batch).
     """
     intervals = np.linspace(0, total_news_db-1, num=math.ceil(total_news_db/batch_size), dtype=int)
-
     for i in range(len(intervals)-1):
         yield [intervals[i], intervals[i+1]-1] if i < len(intervals)-2 else [intervals[i], intervals[i+1]]
 
@@ -584,9 +415,9 @@ def _is_news_in_db(news_data, cleaned_news_db, batch):
     results: list
         id_news e valor de Levenshtein se já está registrado, -1 caso contrário.
     """
+    
     try:
         text_processor = TextPreprocessing()
-        # news_data_cleaned = text_processor.text_cleaning(news_data["text_news"])
         text_news_batch = cleaned_news_db[batch[0]:batch[1]]
         results = []
 
