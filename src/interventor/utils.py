498ecd9f852eadb03ec021c3068a828b6c4030f3
Gabriel Machado <gabriel.rmachado10@gmail.com>
2022-06-17 14:14:48 -0300

    Criação do template da planilha em Excel para envio para as ACF.

--- src/interventor/utils.py
+++ src/interventor/utils.py
@@ -24,34 +24,34 @@ def build_excel_sheet(candidates_to_check: list[tuple[int, str]]) -> None:
         with pd.ExcelWriter(file_name, engine='xlsxwriter', mode='w') as writer:
             df_news.to_excel(writer, sheet_name=sheet_name, startrow=2, index=False)
 
-            with writer.book as workbook:
-                worksheet = writer.sheets[sheet_name]
+            workbook = writer.book
+            worksheet = writer.sheets[sheet_name]
+            
+            # add the title to the excel.
+            worksheet.write(0,0, f"NOTÍCIAS PARA CHECAGEM - {dt.datetime.strftime(dt.datetime.now(), '%d/%m/%Y')}", \
+                workbook.add_format({'bold': True, 'font_size': 14, 'align': 'center'}))
+            
+            # format main header
+            merge_format = workbook.add_format({'bold': 1,'border': 1,'align': 'center','valign': 'vcenter', 'font_size': 14, 'fg_color': '#FDE9D9'})
+            
+            worksheet.merge_range('A1:D2', f"NOTÍCIAS PARA CHECAGEM - {dt.datetime.strftime(dt.datetime.now(), '%d/%m/%Y')}", merge_format)
+            
+            # formats the header of the excel.
+            header_format = workbook.add_format({'bold': True, 'font_size': 12, 'align': 'center', 'valign': 'vcenter', 'border': 1})
+            
+            for col_num, value in enumerate(df_news.columns.values):
+                worksheet.write(2, col_num, value, header_format)
                 
-                # add the title to the excel.
-                worksheet.write(0,0, f"NOTÍCIAS PARA CHECAGEM - {dt.datetime.strftime(dt.datetime.now(), '%d/%m/%Y')}", \
-                    workbook.add_format({'bold': True, 'font_size': 14, 'align': 'center'}))
-                
-                # format main header
-                merge_format = workbook.add_format({'bold': 1,'border': 1,'align': 'center','valign': 'vcenter', 'font_size': 14, 'fg_color': '#FDE9D9'})
-                
-                worksheet.merge_range('A1:D2', f"NOTÍCIAS PARA CHECAGEM - {dt.datetime.strftime(dt.datetime.now(), '%d/%m/%Y')}", merge_format)
-                
-                # formats the header of the excel.
-                header_format = workbook.add_format({'bold': True, 'font_size': 12, 'align': 'center', 'valign': 'vcenter', 'border': 1})
-                
-                for col_num, value in enumerate(df_news.columns.values):
-                    worksheet.write(2, col_num, value, header_format)
-                    
-                # add border to the table.
-                border_fmt = workbook.add_format({'bottom':1, 'top':1, 'left':1, 'right':1})
-                worksheet.conditional_format(xlsxwriter.utility.xl_range(0, 0, len(df_news)+2, len(df_news.columns)-1), {'type': 'no_errors', 'format': border_fmt})
-                
-                # center the values of the worksheet, except the second.
-                format = workbook.add_format({'align': 'center'})
-                
-                # set column widths.
-                worksheet.set_column(0, 0, 16, format)
-                worksheet.set_column(1, 1, 250)
-                worksheet.set_column(2, 3, 30, format)
+            # add border to the table.
+            border_fmt = workbook.add_format({'bottom':1, 'top':1, 'left':1, 'right':1})
+            worksheet.conditional_format(xlsxwriter.utility.xl_range(0, 0, len(df_news)+2, len(df_news.columns)-1), {'type': 'no_errors', 'format': border_fmt})
+            
+            # center the values of the worksheet, except the second.
+            format = workbook.add_format({'align': 'center'})
+            
+            # set column widths.
+            worksheet.set_column(0, 0, 16, format)
+            worksheet.set_column(1, 1, 250)
+            worksheet.set_column(2, 3, 30, format)
                 
         return f"Planilha de notícias para checagem gerada com sucesso em {file_name}."
